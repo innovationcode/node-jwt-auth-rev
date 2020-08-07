0e9391a964d2eb5cdfdf1995fa3ef0e4a0430fab
lajawanti <lajawanti.dhake@gmail.com>
2020-08-06 23:39:56 -0400

    body-parser and cors

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const app = express();
 
-const app = express()
+var corsOptions = {
+    origin: 'http://localhost:8081'
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.json({
@@ -10,6 +22,7 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT || 7000;
-app.listen((PORT, () => {
-    console.log(`Server running on PORT : ${PORT}`)
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT : ${PORT}`);
 })
